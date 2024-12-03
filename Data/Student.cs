e8344e13e6beb6dedf3552c9f3cb3ff91e04cc96
daryaanbar <anbardarya22@gmail.com>
2024-12-03 12:36:51 -0600

    refactored folders

--- Data/Student.cs
+++ Data/Student.cs
@@ -1,3 +1,11 @@
+/*
+    Written by Kiara Vaz for CS 4485.0W1, Senior Design Project, Started October 20, 2024.
+    Net ID: kmv200000
+
+    This file defines the Student class, which models the essential information for a student in the system.
+    The class includes properties for the student's identification, login credentials, course section, and team assignment.
+*/
+
 using System.ComponentModel.DataAnnotations;
 
 namespace StudentPeerReview.Models
---
+++ Models/Privacy.cshtml.cs
@@ -0,0 +1,29 @@
+﻿/*
+    Written by Kiara Vaz for CS 4485.0W1, Senior Design Project, Started October 20, 2024.
+    Net ID: kmv200000
+
+    This file defines the model for viewing the application's Privacy Policy.
+*/
+
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudentPR.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    // Handles GET requests by returning the Page
+    public IActionResult OnGet()
+    {
+        return Page();
+    }
+}
+
